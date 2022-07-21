0f2d6dfcafcbab82edc8d7ca6d70de3c54020fab
Aleksey Studenov <aleksey.studenov@smissltd.com>
2022-07-21 18:29:42 +0300

    layouts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import {Footer} from './layout/Footer'
+import {Header} from './layout/Header'
+import {Main} from './layout/Main'
+
 function App() {
   return (
-    <div className="App">
-      asasa
-    </div>
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
   );
 }
 
