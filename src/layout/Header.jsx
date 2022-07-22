03eccd34a09d5a419c7e67ff9e7a3a67900df319
Aleksey Studenov <aleksey.studenov@smissltd.com>
2022-07-22 12:06:45 +0300

    fixes

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
     return <nav className="green darken-1">
         <div className="nav-wrapper">
-        <a href="/" className="brand-logo">React Movies</a>
+        <a href="#" className="brand-logo">React Movies</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="!#">Repo</a></li>
+            <li><a href="#">Repo</a></li>
         </ul>
         </div>
     </nav>
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,6 +3,7 @@ import {Movies} from '../components/Movies';
 import {Search} from '../components/Search';
 import {Preloader} from '../components/Preloader';
 
+const ID_KEY = process.env.REACT_APP_ID_KEY;
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 class Main extends Component {
@@ -12,7 +13,7 @@ class Main extends Component {
     }
 
     componentDidMount() {
-        fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=speed`)
+        fetch(`http://www.omdbapi.com/?i=${ID_KEY}&apikey=${API_KEY}&s=speed`)
         .then(response => response.json())
         .then(data => this.setState({
             movies: data.Search,
@@ -24,10 +25,10 @@ class Main extends Component {
         });
     }
 
-    searchMovies = (search, type = 'all') => {
+    searchMovies = (search = 'Yellowstone', type = 'all') => {
         this.setState({ loading: true });
         fetch(
-            `https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}${
+            `https://www.omdbapi.com/?i=${ID_KEY}&apikey=${API_KEY}&s=${search}${
                 type !== 'all' ? `&type=${type}` : ''
             }`
         )
